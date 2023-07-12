99317d8243101d05d63d4b7bfbb6b84c3ef9313c
sanj222 <134222801+sanj222@users.noreply.github.com>
2023-07-12 22:52:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
-// your code here
+if (num === 0) {
+    return 0;
+  }
+  
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+  
+  return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
