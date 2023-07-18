58808a774b74808007949f2ffb50923d8bb8fa4c
sanj222 <134222801+sanj222@users.noreply.github.com>
2023-07-18 12:52:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 function fibonacci(num) {
-if (num === 0) {
-    return 0;
-  }
-  
-  if (num === 1 || num === 2) {
-    return 1;
-  }
-  
-  return fibonacci(num - 1) + fibonacci(num - 2);
+// your code here
+	if(num === 1) return 0;
+	if(num === 2) return 1;
+
+	return fibonacci(num-2)+fibonacci(num-1);
 }
 
-let result=fibonacci();
-console.log(result);
+module.exports = fibonacci;
